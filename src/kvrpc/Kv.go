3e7bfc6df97b3924b0c2e8c29c0410426bcbacd1
SpirtCrazy <415131215@qq.com>
2017-09-18 23:52:22 +0800

    rpc

--- src/kvrpc/Kv.go
+++ src/kvrpc/Kv.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"net/rpc"
+	"net"
 )
 
 const (
@@ -36,7 +38,7 @@ type KV struct {
 	keyvalue map[string]string
 }
 
-func (kv *KV)Get(args *GetArgs, reply *GetReply)  {
+func (kv *KV)Get(args *GetArgs, reply *GetReply) error {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 
@@ -63,5 +65,63 @@ func (kv *KV)Put(args *PutArgs, reply *PutReply) error {
 
 func server()  {
 	kv := new(KV)
-	
+	kv.keyvalue = map[string]string{}
+	rpcs := rpc.NewServer()
+	rpcs.Register(kv)
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+
+	go func() {
+		for  {
+			conn, err := l.Accept()
+			if err == nil {
+				go rpcs.ServeConn(conn)
+			} else {
+				break
+			}
+		}
+		l.Close()
+		fmt.Println("Server done\n")
+	}()
+}
+
+func Dial() *rpc.Client {
+	client, err := rpc.Dial("tcp", ":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	return client
+}
+
+func Get(key string) string {
+	client := Dial()
+	args := &GetArgs{"subject"}
+	reply := GetReply{"", ""}
+	err := client.Call("KV.Get", args, &reply)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+	client.Close()
+	return reply.Value
+}
+
+func Put(key string, val string)  {
+	client := Dial()
+	args := &PutArgs{"subject", "6.824"}
+	reply := PutReply{""}
+	err := client.Call("KV.Put", args, &reply)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+	client.Close()
+}
+
+func main()  {
+	server()
+
+	Put("subject", "6.824")
+	fmt.Printf("Put subjet done\n")
+	fmt.Printf("Get %s\n", Get("subject"))
 }
